57bd9fda8ae50472c338ed873b13a2bc332eb7c6
decamel <den_spb97@mail.ru>
2023-10-16 11:46:51 +0300

    fix:wreorder

--- wellnmea/sentence.hpp
+++ wellnmea/sentence.hpp
@@ -1,64 +1,55 @@
 #pragma once
 
 /* ----------------------------------- STL ---------------------------------- */
-#include <string>
-#include <optional>
 #include <algorithm>
 #include <list>
+#include <optional>
 #include <stdexcept>
+#include <string>
 
-namespace wellnmea
-{
-  class Sentence
-  {
-  private:
-    bool isValid_;
+namespace wellnmea {
+class Sentence {
+ private:
+  bool isValid_;
 
-  public:
-    /// @brief full raw text of received message between $ and * symbols
-    std::string text;
-    /// @brief message talker
-    std::string_view talker;
-    /// @brief message formatter
-    std::string_view formatter;
+ public:
+  /// @brief full raw text of received message between $ and * symbols
+  std::string text;
+  /// @brief message talker
+  std::string_view talker;
+  /// @brief message formatter
+  std::string_view formatter;
 
-    /// @brief parsed checksum field content
-    std::string_view checksumField;
+  /// @brief parsed checksum field content
+  std::string_view checksumField;
 
-    /// @brief List of parsed fields
-    std::list<std::string_view> fields; // list + string_view choosed by results of benchmark/list_to_vector
+  /// @brief List of parsed fields
+  std::list<std::string_view>
+      fields;  // list + string_view choosed by results of benchmark/list_to_vector
 
-    /// @brief calculated checksum of text property
-    uint8_t payloadChecksum;
-    /// @brief digit viewof checksum property
-    uint8_t parsedChecksum;
+  /// @brief calculated checksum of text property
+  uint8_t payloadChecksum;
+  /// @brief digit viewof checksum property
+  uint8_t parsedChecksum;
 
-  public:
-    /**
+ public:
+  /**
      * @brief NMEA Sentence empty object
      *
      */
-    Sentence() : isValid_(false), parsedChecksum(0), payloadChecksum(0) {}
+  Sentence() : isValid_(false), payloadChecksum(0), parsedChecksum(0) {}
 
-  public:
-    /**
+ public:
+  /**
      * @brief Marks this sentence as valid NMEA sentence
      */
-    void markValid()
-    {
-      isValid_ = true;
-    }
+  void markValid() { isValid_ = true; }
 
-    /**
+  /**
      * @brief Verifies that calculated payload checksum is equal to parsed one
      */
-    bool integrity() const {
-      return payloadChecksum == parsedChecksum;
-    }
+  bool integrity() const { return payloadChecksum == parsedChecksum; }
 
-    bool isValid() const
-    {
-      return isValid_;
-    }
-  };
-} // namespace wellnmea
+  bool isValid() const { return isValid_; }
+};
+}  // namespace wellnmea
