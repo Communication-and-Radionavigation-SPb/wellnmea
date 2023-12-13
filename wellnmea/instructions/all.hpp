f7b2b70abd812fc0da6d55ef5afed7271a4a2844
decamel <den_spb97@mail.ru>
2023-12-13 13:01:57 +0300

    Added single import point for all instructions

---
+++ wellnmea/instructions/all.hpp
@@ -0,0 +1,18 @@
+#if !defined(WELLNMEA_INSTRUCTIONS_ALL_HPP)
+#define WELLNMEA_INSTRUCTIONS_ALL_HPP
+
+#include <wellnmea/instrucitons/utc_instruction.hpp>
+#include <wellnmea/instructions/as_is_instruction.hpp>
+#include <wellnmea/instructions/coordinate_instruction.hpp>
+#include <wellnmea/instructions/date_instruction.hpp>
+#include <wellnmea/instructions/degrees_instruction.hpp>
+#include <wellnmea/instructions/distance_instruction.hpp>
+#include <wellnmea/instructions/latitude_instruction.hpp>
+#include <wellnmea/instructions/longitude_instruction.hpp>
+#include <wellnmea/instructions/number_instruction.hpp>
+#include <wellnmea/instructions/repeated_instruction.hpp>
+#include <wellnmea/instructions/skip_empty_instruction.hpp>
+#include <wellnmea/instructions/speed_instruction.hpp>
+#include <wellnmea/instructions/symbol_instruction.hpp>
+
+#endif  // WELLNMEA_INSTRUCTIONS_ALL_HPP
