fd3a559f1f2c4b11b7e6a2140747c52f97678c2c
decamel <den_spb97@mail.ru>
2024-02-06 14:16:05 +0300

    degrees instruction value name binding

--- wellnmea/instructions/degrees_instruction.hpp
+++ wellnmea/instructions/degrees_instruction.hpp
@@ -4,66 +4,56 @@
 #include <wellnmea/util/number_utils.hpp>
 #include <wellnmea/util/string_utils.hpp>
 
-namespace wellnmea
-{
+namespace wellnmea {
 
-  namespace instructions
-  {
+namespace instructions {
 
-    /* -------------------------------------------------------------------------- */
-    /*                                Degrees Value                               */
-    /* -------------------------------------------------------------------------- */
-    class DegreesValue : public NullValue
-    {
-    public:
-      using visitor_t = visitor<DegreesValue>;
-      /* ------------------------------- Attributes ------------------------------- */
-    public:
-      std::optional<double> value;
+/* -------------------------------------------------------------------------- */
+/*                                Degrees Value                               */
+/* -------------------------------------------------------------------------- */
+class DegreesValue : public NullValue {
+ public:
+  using visitor_t = visitor<DegreesValue>;
+  /* ------------------------------- Attributes ------------------------------- */
+ public:
+  std::optional<double> value;
 
-    public:
-      DegreesValue() : NullValue("degrees") {}
+ public:
+  DegreesValue(std::string const& name = "degrees") : NullValue(name) {}
 
-    public:
-      virtual void accept(visitor_base &v) const noexcept override
-      {
-        visitor_t *ev = dynamic_cast<visitor_t *>(&v);
-        if (ev != nullptr)
-          ev->visit(this);
-      }
-    };
+ public:
+  virtual void accept(visitor_base& v) const noexcept override {
+    visitor_t* ev = dynamic_cast<visitor_t*>(&v);
+    if (ev != nullptr)
+      ev->visit(this);
+  }
+};
 
-    /* -------------------------------------------------------------------------- */
-    /*                                 Instruction                                */
-    /* -------------------------------------------------------------------------- */
-    class DegreesInstruction : public Instruction
-    {
-    public:
-      DegreesInstruction(const std::string &name) : Instruction(name) {}
+/* -------------------------------------------------------------------------- */
+/*                                 Instruction                                */
+/* -------------------------------------------------------------------------- */
+class DegreesInstruction : public Instruction {
+ public:
+  DegreesInstruction(const std::string& name) : Instruction(name) {}
 
-    public:
-      std::string which() const noexcept override
-      {
-        return "degrees";
-      }
+ public:
+  std::string which() const noexcept override { return "degrees"; }
 
-      value *extract(position it, const_position end) override
-      {
-        auto value = new DegreesValue();
+  value* extract(position it, const_position end) override {
+    auto value = new DegreesValue(name());
 
-        // Assign degrees value
-        value->value = util::toDouble(std::string{it->begin(), it->end()});
+    // Assign degrees value
+    value->value = util::toDouble(std::string{it->begin(), it->end()});
 
-        // Move iterator forward
-        it++;
-        return value;
-      }
+    // Move iterator forward
+    it++;
+    return value;
+  }
 
-      Instruction *clone(const std::string &name) const override
-      {
-        return new DegreesInstruction(name);
-      }
-    };
-  } // namespace instructions
+  Instruction* clone(const std::string& name) const override {
+    return new DegreesInstruction(name);
+  }
+};
+}  // namespace instructions
 
-} // namespace wellnmea
+}  // namespace wellnmea
