27dc6945a949650393f12dc023f69f1b3ac4f937
decamel <den_spb97@mail.ru>
2024-04-12 20:32:49 +0300

    qualifier for transpiler income sentence

--- wellnmea/transpiler.hpp
+++ wellnmea/transpiler.hpp
@@ -1,41 +1,36 @@
 #pragma once
 
-#include <memory>
 #include <list>
-#include <unordered_map>
+#include <memory>
 #include <string>
+#include <unordered_map>
 
 #include <wellnmea/util/string_utils.hpp>
 
 #include <wellnmea/exceptions.hpp>
-#include <wellnmea/message.hpp>
 #include <wellnmea/formats/format.hpp>
+#include <wellnmea/message.hpp>
 
 using namespace wellnmea::formats;
 
-namespace wellnmea
-{
-  class Transpiler
-  {
-  public:
-    using FmtPtr = std::shared_ptr<Format>;
+namespace wellnmea {
+class Transpiler {
+ public:
+  using FmtPtr = std::shared_ptr<Format>;
 
-  private:
-    std::unordered_map<std::string, FmtPtr> m_registry;
+ private:
+  std::unordered_map<std::string, FmtPtr> m_registry;
 
-  public:
-    Transpiler() : m_registry({}) {}
+ public:
+  Transpiler() : m_registry({}) {}
 
-  public:
-    /**
+ public:
+  /**
      * @brief Drops all connected formats
      */
-    void clear()
-    {
-      m_registry.clear();
-    }
-    /* -------------------------------------------------------------------------- */
-    /**
+  void clear() { m_registry.clear(); }
+  /* -------------------------------------------------------------------------- */
+  /**
      * @brief Checks if parser has format with fmt_name
      * connected
      *
@@ -43,12 +38,11 @@ namespace wellnmea
      * @return true If parsed has registered format
      * @return false otherwise
      */
-    bool contains(const std::string &fmt_name) const noexcept
-    {
-      return m_registry.find(fmt_name) != m_registry.end();
-    }
-    /* -------------------------------------------------------------------------- */
-    /**
+  bool contains(const std::string& fmt_name) const noexcept {
+    return m_registry.find(fmt_name) != m_registry.end();
+  }
+  /* -------------------------------------------------------------------------- */
+  /**
      * @brief Connects parser with format which has fmt_name
      * to be used for further parsing
      *
@@ -59,17 +53,15 @@ namespace wellnmea
      * part of NMEA message
      * @param fmt Format object
      */
-    void connect(std::string fmt_name, FmtPtr fmt)
-    {
-      if (contains(fmt_name))
-      {
-        throw format_duplication(fmt_name);
-      }
-      auto key = util::to_lower(fmt_name);
-      m_registry.emplace(key, fmt);
+  void connect(std::string fmt_name, FmtPtr fmt) {
+    if (contains(fmt_name)) {
+      throw format_duplication(fmt_name);
     }
-    /* -------------------------------------------------------------------------- */
-    /**
+    auto key = util::to_lower(fmt_name);
+    m_registry.emplace(key, fmt);
+  }
+  /* -------------------------------------------------------------------------- */
+  /**
      * @brief Returns format associated with provided format name
      *
      * Returns format rules for format name.
@@ -77,29 +69,26 @@ namespace wellnmea
      * @param fmt_name Name of format
      * @return FmtPtr Shared pointer to format rules object
      */
-    FmtPtr getFormat(std::string fmt_name) const
-    {
-      if (!contains(fmt_name))
-      {
-        throw format_unregistered(fmt_name);
-      }
-      return m_registry.at(fmt_name);
+  FmtPtr getFormat(std::string fmt_name) const {
+    if (!contains(fmt_name)) {
+      throw format_unregistered(fmt_name);
     }
+    return m_registry.at(fmt_name);
+  }
 
-    /* -------------------------------------------------------------------------- */
-    /**
+  /* -------------------------------------------------------------------------- */
+  /**
      * @brief Removes format from parser
      *
      * @param fmt_name Format name to be removed
      */
-    void dropFormat(std::string fmt_name) noexcept
-    {
-      if (!contains(fmt_name))
-        return;
-      m_registry.erase(fmt_name);
-    }
-    /* -------------------------------------------------------------------------- */
-    /**
+  void dropFormat(std::string fmt_name) noexcept {
+    if (!contains(fmt_name))
+      return;
+    m_registry.erase(fmt_name);
+  }
+  /* -------------------------------------------------------------------------- */
+  /**
      * @brief Dumps names of connected formats
      *
      * Returns connected format names joined with "|" characted
@@ -109,19 +98,17 @@ namespace wellnmea
      *
      * @return std::string format names joined with "|"
      */
-    std::string dumpRegistered() noexcept
-    {
-      std::string dump = "";
-      for (auto it = m_registry.begin(); it != m_registry.end(); it++)
-      {
-        dump += it->first;
-        dump += "|";
-      }
-      return dump;
+  std::string dumpRegistered() noexcept {
+    std::string dump = "";
+    for (auto it = m_registry.begin(); it != m_registry.end(); it++) {
+      dump += it->first;
+      dump += "|";
     }
+    return dump;
+  }
 
-    /* -------------------------------------------------------------------------- */
-    /**
+  /* -------------------------------------------------------------------------- */
+  /**
      * @brief Parses provided source NMEA0183 string
      *
      * @throws parse_error In case if provided source string
@@ -129,18 +116,17 @@ namespace wellnmea
      * @param source Source string containing
      * @return NmeaMessage Parsed message
      */
-    std::shared_ptr<Message> parse(Sentence &source) const
-    {
-      auto formatter =
-          util::to_lower(std::string{source.formatter.begin(), source.formatter.end()});
+  std::shared_ptr<Message> parse(Sentence const& source) const {
+    auto formatter = util::to_lower(
+        std::string{source.formatter.begin(), source.formatter.end()});
 
-      auto fmt = getFormat(formatter);
+    auto fmt = getFormat(formatter);
 
-      auto parsed = fmt->parse(source);
+    auto parsed = fmt->parse(source);
 
-      auto talker = std::string{source.talker.begin(), source.talker.end()};
+    auto talker = std::string{source.talker.begin(), source.talker.end()};
 
-      return std::shared_ptr<Message>(new Message(talker, formatter, parsed));
-    }
-  };
-} // namespace wellnmea
+    return std::shared_ptr<Message>(new Message(talker, formatter, parsed));
+  }
+};
+}  // namespace wellnmea
